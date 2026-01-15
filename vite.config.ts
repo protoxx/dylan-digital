b83754f621c00cf5c300ab1eb99c8b0cb2f2384a
protoxx <ffbondoufle@gmail.com>
2026-01-15 12:29:03 +0100

    opti

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react(), imagetools()],
+      build: {
+        cssCodeSplit: false
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
